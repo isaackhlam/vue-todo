8484f943feb1fb88aaad0757671143afa3344355
isaackhlam <97761927+isaackhlam@users.noreply.github.com>
2023-05-30 20:07:32 +0800

    wip 1

---
+++ vue-todos/src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+    todo: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <li>
+        <input type="checkbox" :checked="todo.isCompleted"/>
+        <div class="todo">
+            <input type="text">
+            <span>
+                {{ todo.todo }}
+            </span>
+        </div>
+    </li>
+</template>
+
+<style lang="scss" scoped>
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+</style>
